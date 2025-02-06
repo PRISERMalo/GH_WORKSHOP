542652bd87dbafab14726bd0f1264a0b9bed328c
PRISERMalo <malo.priser@epitech.eu>
2025-02-06 14:41:24 +0400

    [test cs action]

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** GH_WORKSHOP
+** File description:
+** main
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
-    // char *memory_leak = malloc(sizeof(char) * 10);
     printf("Hello world");
 }
